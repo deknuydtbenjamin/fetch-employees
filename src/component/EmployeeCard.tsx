cc342771a1b1dfbd756f4145635dd41b0c9ba6f3
Benjamin Deknuydt <deknuydtbenjamin@hotmail.fr>
2024-10-30 16:35:07 +0100

    first commit

---
+++ src/component/EmployeeCard.tsx
@@ -0,0 +1,26 @@
+type EmployeeCardProps = {
+	employee: {
+		name: {
+			first: string;
+			last: string;
+		};
+		email: string;
+		picture: {
+			medium: string;
+		};
+	};
+};
+
+export default function EmployeeCard({ employee }: EmployeeCardProps) {
+	return (
+		<figure className="DisplayEmployee">
+			<img src={employee.picture.medium} alt={employee.name.first} />
+			<figcaption>
+				<strong>
+					{employee.name.first} {employee.name.last}
+				</strong>
+				{employee.email}
+			</figcaption>
+		</figure>
+	);
+}
